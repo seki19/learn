d93975af1d498c445f6bc27009f5ed0c9bccc8f2
関郁也 <seki.198.70@gmail.com>
2022-02-12 18:51:25 +0900

    TODOリストを返すAPI,TODOを作成するAPIを実装

--- api/main.py
+++ api/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
+
+from api.routers import task, done
+
+
 app = FastAPI()
-
-
-@app.get("/hello")
-async def hello():
-    return {"message": "hello wrold!"}
+app.include_router(task.router)
+app.include_router(done.router)
